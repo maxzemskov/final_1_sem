5ee4886f596a75bcb08043d8c16cb74388f8220a
mzemskov <mzemskov@dimax.digital>
2022-12-26 17:16:56 +0300

    task_done

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите массив: ");
+string[] input = Console.ReadLine().Split(" ");
+int i = 0;
+string[] final = new string[input.Length];
+int j = 0;
+while (i < input.Length)
+{
+    if (input[i].Length < 4)
+    {
+        final[j] = input[i];
+        i++;
+        j++;
+    }
+    else
+    {
+        i++;
+    }
+    }
+Console.Write("Output: ");
+Console.WriteLine(string.Join(" ", final));
